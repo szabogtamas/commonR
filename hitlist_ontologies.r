e9bb36b72a23622d6d928747532c296f8adf378f
Tamas Szabo <szabogtamas@gmail.com>
2020-09-08 13:42:17 +0200

    Reoredered to have main function.

--- hitlist_ontologies.r
+++ hitlist_ontologies.r
@@ -12,29 +12,40 @@ args = commandArgs(trailingOnly=TRUE)
 outFile <- args[1]
 hitGenes <- unlist(strsplit(args[2], ",", fixed=TRUE))
 
+plot_title <- 'Top gene sets'
 msig_species <- "Mus musculus"
 msig_category <- "C5"
 msig_subcategory <- "BP"
 
-geneSet <-msigdbr(species=msig_species, category=msig_category, subcategory=msig_subcategory)
-geneSet$gs_name <- gsub('GO_', '', geneSet$gs_name)
-geneSet$gs_name <- gsub('_', ' ', geneSet$gs_name)
+pAdjustMethod <- "none"
+qvalueCutoff <- 1
 
-hitGenes  %>%
-  enricher(TERM2GENE=geneSet[,c('gs_name', 'gene_symbol')], pAdjustMethod="none", qvalueCutoff=1) %>%
-  clusterProfiler::dotplot(showCategory=30) +
-  labs(title='Biological processes associated') +
+main <- function(){
+  geneSet <-msigdbr(species=msig_species, category=msig_category, subcategory=msig_subcategory)
+  geneSet$gs_name <- gsub('GO_', '', geneSet$gs_name)
+  geneSet$gs_name <- gsub('_', ' ', geneSet$gs_name)
+
+  pdf(outFile)
+  
+  enrichment <- single_enrichment(hitGenes, pAdjustMethod=pAdjustMethod, qvalueCutoff=qvalueCutoff)
+  single_enrichdot(enrichment, plot_title=plot_title))
+  dev.off()
+}
+
+single_enrichment <- function(hitGenes, pAdjustMethod=pAdjustMethod, qvalueCutoff=qvalueCutoff){
+  enricher(hitGenes, TERM2GENE=geneSet[,c('gs_name', 'gene_symbol')], pAdjustMethod=pAdjustMethod, qvalueCutoff=qvalueCutoff)
+}
+
+single_enrichdot <- function(enrichment, plot_title=plot_title){
+  clusterProfiler::dotplot(enrichment, showCategory=30) +
+  labs(title=plot_title) +
   scale_color_gradientn(
     colors=rev(c('#2b8cbe', 'grey', '#e38071', '#e34a33', '#e31e00')),
     breaks=c(0.05, 0.01, 0.001, 0.0001),
     limits=c(0.00001, 1), trans='log10', oob = scales::squish) +
   theme(axis.text.x=element_text(angle=30, hjust=1))
+}
 
-pdf(outFile)
-
-enricher(hitGenes, TERM2GENE=geneSet[,c('gs_name', 'gene_symbol')], pAdjustMethod="none",)  %>%
-  clusterProfiler::dotplot(showCategory=30) + labs(title='Gene ontologies\nassociated with\nhit genes') +
-  scale_color_gradientn(colors=rev(c('#2b8cbe', 'grey', '#e38071', '#e34a33', '#e31e00')), breaks=c(0.05, 0.01, 0.001, 0.0001), limits=c(0.00001, 1), trans='log10', oob = scales::squish) +
-  theme(axis.text.x=element_text(angle=30, hjust=1))
-
-dev.off()
+if (!interactive()) {
+  main()
+}
