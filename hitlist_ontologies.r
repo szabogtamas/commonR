33afec6ce62ae3deac246246edaae0f521106ea9
Tamas Szabo <szabogtamas@gmail.com>
2020-09-08 13:31:55 +0200

    Finetune parameters

--- hitlist_ontologies.r
+++ hitlist_ontologies.r
@@ -12,10 +12,23 @@ args = commandArgs(trailingOnly=TRUE)
 outFile <- args[1]
 hitGenes <- unlist(strsplit(args[2], ",", fixed=TRUE))
 
+msig_species <- "Mus musculus"
+msig_category <- "C5"
+msig_subcategory <- "BP"
 
-geneSet <-msigdbr(species="Mus musculus", category="C5", subcategory="BP")
+geneSet <-msigdbr(species=msig_species, category=msig_category, subcategory=msig_subcategory)
 geneSet$gs_name <- gsub('GO_', '', geneSet$gs_name)
 geneSet$gs_name <- gsub('_', ' ', geneSet$gs_name)
+
+hitGenes  %>%
+  enricher(TERM2GENE=geneSet[,c('gs_name', 'gene_symbol')], pAdjustMethod="none", qvalueCutoff=1) %>%
+  clusterProfiler::dotplot(showCategory=30) +
+  labs(title='Biological processes associated') +
+  scale_color_gradientn(
+    colors=rev(c('#2b8cbe', 'grey', '#e38071', '#e34a33', '#e31e00')),
+    breaks=c(0.05, 0.01, 0.001, 0.0001),
+    limits=c(0.00001, 1), trans='log10', oob = scales::squish) +
+  theme(axis.text.x=element_text(angle=30, hjust=1))
 
 pdf(outFile)
 
