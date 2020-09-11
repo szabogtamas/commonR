684b5a0cc826c726271f07e1fa400d7a54fa2e7a
Tamas Szabo <szabogtamas@gmail.com>
2020-09-11 13:50:32 +0200

    Added tested version of command line parsing

--- hitlist_ontologies.r
+++ hitlist_ontologies.r
@@ -1,8 +1,18 @@
 #!/usr/bin/env Rscript
 
-# A script that takes a hitlist and shows top gene ontologies
+scriptDescription <- "A script that takes a hitlist and shows top gene ontologies"
 
-scriptMandatoryArgs <- c("hitGenes", "outFile")
+scriptMandatoryArgs <- list(
+  hitGenes = list(
+    abbr="-i",
+    type="nested",
+    help="A comma separated list of genes, usually a hitlist."
+  ),
+  outFile = list(
+    abbr="-o",
+    help="Prefix for output files."
+  )
+)
 
 scriptOptionalArgs <- list(
   plot_title = list(
@@ -29,52 +39,19 @@ scriptOptionalArgs <- list(
     default=1,
     help="An FDR of 0.05 is usually too stringent."
   )
+)
 
-suppressPackageStartupMessages(library("optparse"))
-suppressPackageStartupMessages(library("docstring"))
+suppressPackageStartupMessages(library(optparse))
+suppressPackageStartupMessages(library(docstring))
 
-library(tidyr)
-library(ggplot2)
-library(cowplot)
-library(msigdbr)
-library(clusterProfiler)
-
-
-
-
-if (!interactive()) {
-  # Parse command line options if not sourced
-  parser <- OptionParser()
-  parser <- add_option(parser, c("-v", "--verbose"), action="store_true", default=FALSE,
-                      help="Print some progress messages to stdout.")
-  parser <- add_option(parser, c("-q", "--quietly"), action="store_false", 
-                      dest="verbose", help="Create figures quietly, without printing to stdout.")
-  parser <- add_option(parser, c("-i", "--hitGenes"), default=NULL, 
-                      help="Comma separated list of hit genes.",
-                      metavar="hit_genes")
-  parser <- add_option(parser, c("-o", "--outFile"), default=NULL, 
-                      help="File name prefix where to save output plots.",
-                      metavar="msig_category")
-  parser <- add_option(parser, c("-o", "--outFile"), default=NULL, 
-                      help="File name prefix where to save output plots.",
-                      metavar="msig_subcategory")
-  parser <- add_option(parser, c("-o", "--plot_title"), default=NULL, 
-                      help="File name prefix where to save output plots.",
-                      metavar="out_prefix")
-  parser <- add_option(parser, c("-o", "--msig_species"), default=NULL, 
-                      help="File name prefix where to save output plots.",
-                      metavar="out_prefix")
-  parser <- add_option(parser, c("-o", "--outFile"), default=NULL, 
-                      help="File name prefix where to save output plots.",
-                      metavar="out_prefix")
-  parser <- add_option(parser, c("-o", "--outFile"), default=NULL, 
-                      help="File name prefix where to save output plots.",
-                      metavar="out_prefix")
-  opt <- parse_args(parser)
-
-  print(opt)
+### Define a main function that will only be executed if called from command line
+main <- function(verbose, ...){
+  my_example <- test_helper(...)
+  if(verbose){
+    print(my_example)
+  }
+  
 }
-
 ### Define a main function that will only be executed if called from command line
 main <- function(){
   geneSet <-msigdbr(species=msig_species, category=msig_category, subcategory=msig_subcategory)
@@ -229,14 +206,89 @@ plotMultipleEnrichments <- function(
 
 if (!interactive()) {
   
+  # Initialize parser with verbosity and description of script
+  parser <- OptionParser(usage=paste0("%prog [options]\nDescription:\n  ", scriptDescription))
+  parser <- add_option(
+    parser,
+    c("-v", "--verbose"),
+    action="store_true",
+    default=FALSE,
+    help="Print some progress messages to stdout."
+    )
+  parser <- add_option(
+    parser,
+    c("-q", "--quietly"),
+    action="store_false",
+    dest="verbose",
+    help="Create figures quietly, without printing to stdout."
+    )
+
+  # Add arguments to parser
+  an <- 0
+  for (al in list(scriptMandatoryArgs, scriptOptionalArgs)){
+    for (rgn in names(al)){
+      rg <- al[[rgn]]
+      if (an < 1){
+        rg[["default"]] <- NULL
+      }
+
+      rga <- paste0("--", rgn)
+      if ("abbr" %in% names(rg) ) {
+        rga <- c(rg[["abbr"]], rga)
+        rg[["abbr"]] <- NULL
+      }
+
+      if ("type" %in% names(rg) ) {
+        rg[["type"]] <- NULL
+      }
+
+      rl <- list(parser, rga)
+      rl <- c(rl, rg)
+      parser <- do.call(add_option, rl)
+      an <- an +1
+    }
+  }
+
+  # Parse command line options and split up lists or nested lists
+  opt <- parse_args(parser)
+  for (rn in names(c(scriptMandatoryArgs, scriptOptionalArgs))){
+    rg <- c(scriptMandatoryArgs, scriptOptionalArgs)[[rn]]
+    if ("type" %in% names(rg) ) {
+        if (rg[["type"]] %in% c("vector", "nested") ) {
+          if (rg[["type"]] == "vector") {
+            opt[[rn]] <- unlist(strsplit(opt[[rn]], ",", fixed=TRUE))
+          } else {
+            nl <- list()
+            for (x in unlist(strsplit(opt[[rn]], ":", fixed=TRUE))){
+              x <- unlist(strsplit(x, ",", fixed=TRUE))
+              nl[[x[1]]] <- x[2:length(x)]
+            }
+            opt[[rn]] <- nl
+          }
+        }
+      }
+  }
+  
+
   # Check if mandatory arguments are present
-  if ( is.null(opt$inputfile) ) { 
-    if ( opt$verbose ) { 
-      write("Sorry, cannot proceed without a data table. Please provide a path to hitlists.\n", stderr())
+  passed_args <- opt[names(scriptMandatoryArgs)]
+  if (any(is.na(names(passed_args)))) {
+    if (opt$verbose) { 
+      write("Sorry, cannot proceed without all mandatory arguments.\n", stderr())
     }
     checkpass <- FALSE
   } else {
     checkpass <- TRUE
+  }
+
+  # Execute main function if mandatory arguments are set (otherwise print help message)
+  if (checkpass) { 
+    do.call(main, opt)
+  } else {
+    print_help(parser)
+  }
+  
+}kpass <- TRUE
   }
 
   # Execute main function if mandatory arguments set (otherwise print help message)
