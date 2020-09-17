6126809b7fc9d18b0792403e508ef65bc45527b0
Tamas Szabo <szabogtamas@gmail.com>
2020-09-17 21:47:14 +0200

    Forgot to import ggplotify

--- hitlist_ontologies.r
+++ hitlist_ontologies.r
@@ -1,7 +1,5 @@
 #!/usr/bin/env Rscript
 
-devtools::install_github('GuangchuangYu/ggplotify')
-
 scriptDescription <- "A script that takes a hitlist and shows top gene ontologies"
 
 scriptMandatoryArgs <- list(
@@ -60,7 +58,7 @@ for (rn in names(scriptOptionalArgs)){
   opt[[rn]] <- scriptOptionalArgs[[rn]][["default"]]
 }
 
-for (pk in c("tidyr", "dplyr", "ggplot2", "cowplot", "msigdbr", "clusterProfiler", "rlang")){
+for (pk in c("tidyr", "dplyr", "ggplot2", "ggplotify", "cowplot", "msigdbr", "clusterProfiler", "rlang")){
   if(!(pk %in% (.packages()))){
     library(pk, character.only=TRUE)
   }
