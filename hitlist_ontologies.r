3efdea29c2c7ecdb282a6dbd9cc866031f6a292a
Tamas Szabo <szabogtamas@gmail.com>
2020-09-18 20:43:23 +0200

    Fixing color scale name

--- hitlist_ontologies.r
+++ hitlist_ontologies.r
@@ -523,7 +523,7 @@ multi_hitlist_genedot <- function(enrichment, cohort_order=NULL){
     scale_x_continuous(breaks=seq(1, length(unique(geneFuns$geneID))), labels = unique(arrange(geneFuns, by=gene)$geneID), "") +
     scale_y_continuous(breaks=seq(1, length(unique(geneFuns$ID))), labels = unique(arrange(geneFuns, by=geneSet)$ID), "") +
     #geom_scatterpie_legend(geneFuns$ScaledGeneSetSize, x=1, y=1) +
-    #scale_colour_manual(values = c("red", "blue", "green"), name="") +
+    scale_colour_manual(values = c("red", "blue", "green"), aesthetics="fill", name="") +
     coord_equal() +
     theme(
       axis.text.x=element_text(size=7, angle=30, hjust=1),
