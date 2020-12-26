9e315c058dc67d613554375bdf1db63aff5b80b1
Tamas Szabo <szabogtamas@gmail.com>
2020-12-26 16:00:32 +0100

    Added PROGENy caller script

--- progeny_paths.r
+++ progeny_paths.r
@@ -51,20 +51,18 @@ for (rn in names(scriptOptionalArgs)){
   opt[[rn]] <- scriptOptionalArgs[[rn]][["default"]]
 }
 
-for (pk in c("tidyr", "dplyr", "purrr", "tibble", "openxlsx", "edgeR", "pheatmap", "EnhancedVolcano", "ggplot2", "cowplot", "ggplotify")){
+for (pk in c("tidyr", "dplyr", "tibble", "DESeq2", "progeny", "pheatmap", "ggplot2", "cowplot", "ggplotify")){
   if(!(pk %in% (.packages()))){
     library(pk, character.only=TRUE)
   }
 }
-myScratch/DiffExpr/rnaSeqEmuMyc/tables/counts.tsv
-#TODO This script will use Progeny to show which pathways are perturbed. Chack out https://github.com/saezlab/progeny/blob/master/vignettes/progenyBulk.Rmd
 
 #' The main function of the script, executed only if called from command line.
 #' Calls subfunctions according to supplied command line arguments.
 #' 
 #' @param opt list. a named list of all command line options; will be passed on 
 #' 
-#' @return Not intended to return anything, but rather save outputs to files.
+#' @return Not intended to return anything, but rather to save outputs to files.
 main <- function(opt){
   
   outFile <- opt$outFile
@@ -75,28 +73,19 @@ main <- function(opt){
   cat("Testing DE with EdgeR\n")
   progeny_results <- do.call(progenyPathwayScores, opt)
   cat("Saving tables\n")
-  geneDict <- progeny_results$geneDict
-  genetab2tsv(progeny_results$table, outFile)
+  genetab2tsv(progeny_results$table, paste0(outFile, ".tsv"))
   
   cat("Saving figures\n")
-  fignames <- test_results$figures %>%
-    names() %>%
-    gsub("/", "___", .) %>%
-    paste0(outFile, .)
-  map2(
-    test_results$figures,
-    fignames,
-    fig2pdf,
-    height=8.64,
-    width=7.2
-  )
+  p1 <- progeny_results$figures$p1 
+  p2 <- progeny_results$figures$p2
+  p <- plot_grid(p1, p2, ncol=1)
+  fig2pdf(p, paste0(outFile, ".tsv"), height=8.64, width=7.2)
 
   invisible(NULL)
 }
 
 
-#' Runs DE analysis with edger on a count matrix. Condition labels should be in the
-#' order as samples appear in columns.
+#' Run PROGENy on a gene expression matrix to predict activity of canonical pathways.
 #' 
 #' @param readCounts data.frame. The count matrix; first column becoming the index, second labels.
 #' @param conditionLabels character vector. Experimental condition labels. 
@@ -106,34 +95,24 @@ main <- function(opt){
 #' @param clusterSamples If samples should be clustered on the heatmap.
 #' @param ... Arguments inherited from command line but not used by this function. 
 #' 
-#' @return Hitlists.
+#' @return Pathway activity scores.
 progenyPathwayScores <- function(readCounts, conditionLabels, expSpecies, conditionOrder=NULL, conditionColors=NULL, clusterSamples=TRUE, ...){
 
   ### Start by parsing inputs and setting some defaults
   if(is.null(conditionOrder)){
     conditionOrder = unique(conditionLabels)
   }
-  conditionDict <- setNames(conditionLabels, colnames(readCounts[c(-1, -2)]))
     
   conditions <- factor(conditionLabels, levels=conditionOrder)
   
   if (is.null(conditionColors)){
     conditionColors <- default_colors
   }
-  conditionColors <- conditionColors[
-    rep(
-      seq(1, length(conditionColors)),
-      (length(conditionOrder)/length(conditionColors))+1
-    )
-  ]
-  conditionColors <- setNames(conditionColors[1:length(levels(conditions))], levels(conditions))
-  
-  geneDict <- setNames(readCounts[[2]], readCounts[[1]])
 
   metaData <- readCounts %>%
   colnames() %>%
   .[c(-1, -2)] %>%
-  data.frame(group = conditionLabels) %>%
+  data.frame(group = conditions) %>%
   column_to_rownames(var = ".")
 
   ### PROGENy accepts a variance stabilized DEseq2 object as input
@@ -143,7 +122,7 @@ progenyPathwayScores <- function(readCounts, conditionLabels, expSpecies, condit
     group_by(Gene) %>%
     summarise_all(mean) %>%
     ungroup() %>%
-    column_to_rownames(var = "Gene") %>%
+    column_to_rownames(var="Gene") %>%
     DESeqDataSetFromMatrix(colData = metaData, design = ~group) %>%
     estimateSizeFactors() %>%
     estimateDispersions() %>%
@@ -154,92 +133,48 @@ progenyPathwayScores <- function(readCounts, conditionLabels, expSpecies, condit
   
   ### Plot pathway perturbation scores with PROGENy
   p1 <- gexInfo %>%
-    pivot_longer(-SampleID, names_to = "Pathway") %>%
-    left_join(rownames_to_column(metaData, var = "SampleID")) %>%
+    pivot_longer(-SampleID, names_to="Pathway") %>%
+    left_join(rownames_to_column(metaData, var="SampleID")) %>%
     ggplot(aes(x=Pathway, y=value, color=group, fill=group)) + 
-    geom_boxplot(position=position_dodge2(width = 0.8, preserve = "single", padding = 0.2), fill = "white", outlier.shape = NA) +
-    geom_jitter(position=position_jitterdodge(jitter.width = 0.4, dodge.width = 0.8)) +
+    geom_boxplot(position=position_dodge2(width=0.8, preserve="single", padding=0.2), fill="white", outlier.shape=NA) +
+    geom_jitter(position=position_jitterdodge(jitter.width=0.4, dodge.width=0.8), size=.5) +
     scale_color_manual(values=conditionColors, drop=FALSE) +
+    theme_bw() +
     theme(
       axis.ticks = element_blank(),
-      axis.text.x = element_text(size=7, angle=30, hjust=1),
-      legend.position = "right"
+      axis.text.x = element_text(size=10, angle=30, hjust=1),
+      legend.position = "right",
+      legend.title=element_blank()
     ) + 
     labs(x="", y="PROGENy score")
 
-  p2 <- gexInfo %>%
-    pivot_longer(-SampleID, names_to = "Pathway") %>%
-    left_join(rownames_to_column(metaData, var = "SampleID")) %>%
-    ggplot(aes(x=Pathway, y=value, color=group, fill=group)) + 
-    geom_boxplot(position=position_dodge2(width = 0.8, preserve = "single", padding = 0.2), fill = "white", outlier.shape = NA) +
-    geom_jitter(position=position_jitterdodge(jitter.width = 0.4, dodge.width = 0.8)) +
-    scale_color_manual(values=conditionColors, drop=FALSE) +
-    theme(
-      axis.ticks = element_blank(),
-      axis.text.x = element_text(size=7, angle=30, hjust=1),
-      legend.position = "right"
-    ) + 
-    labs(x="", y="PROGENy score")
+  matrixMax <- max(gexInfo[,-1])
+  matrixMin <- min(gexInfo[,-1])
+  matrixRng <- max(matrixMax, abs(matrixMax))
+  matrixBrk <- pretty(-matrixRng:matrixRng, n = 10)
 
   p2 <- gexInfo %>%
   column_to_rownames(var = "SampleID") %>%
   t() %>%
   pheatmap(
-      annotation_colors=list(condition=conditionColors),
+      annotation_col=metaData,
+      annotation_colors=list(group=setNames(conditionColors, conditionOrder)),
       annotation_legend=FALSE,
       annotation_names_row=FALSE,
-      show_colnames=FALSE,
-      show_rownames=TRUE,
-      treeheight_col=0,
-      color=colorRampPalette(c("white", "grey", "red"))(10)
-      ) %>%
-    as.ggplot()
-
-  invisible(list(figures=plots, table=gexInfo))
-
-  annots <- data.frame(condition=as.character(conditions[colnames(normalized_counts)]))
-  rownames(annots) <- colnames(normalized_counts)
-  
-  max_variances <- normalized_counts %>%
-    apply(1, var) %>%
-    order(decreasing=TRUE) %>%
-    .[1:500]
-  
-  opt_color_breaks <- normalized_counts %>%
-    .[max_variances,]  %>%
-    quantile(seq(0.1, 1, 0.1)) %>%
-    as.numeric()
-    
-  p2 <- normalized_counts %>%
-  .[max_variances,]  %>%
-  t() %>%
-  pheatmap(
-      annotation_row=annots,
-      annotation_colors=list(condition=conditionColors),
-      annotation_legend=FALSE,
-      annotation_names_row=FALSE,
-      show_colnames=FALSE,
-      show_rownames=TRUE,
-      treeheight_col=0,
-      color=colorRampPalette(c("white", "grey", "red"))(10),
-      breaks=opt_color_breaks
-      ) %>%
-    as.ggplot()
-
-  normalized_counts  %>%
-    .[rownames(res[1:50,]),samples_to_show] %>%
-    pheatmap(
-      annotation_col=annots,
-      annotation_colors=list(condition=conditionColors),
-      annotation_legend=FALSE,
-      labels_row=geneDict[rownames(res[1:50,])],
       show_colnames=TRUE,
-      cluster_rows=FALSE,
-      cluster_cols = clusterSamples,
-      color=colorRampPalette(c("white", "grey", "red"))(10),
-      breaks=seq(0, 50, 5)
+      show_rownames=TRUE,
+      treeheight_col=0,
+      cluster_cols=clusterSamples,
+      color=colorRampPalette(c("#0000FF", "#1E90FF", "grey", "#FFA07A", "#FF0000"))(length(matrixBrk)),
+      breaks=matrixBrk,
+      display_numbers=TRUE,
+      number_format="%.0f",
+      main="PROGENy pathway activity scores"
       ) %>%
     as.ggplot()
+
+  invisible(list(figures=list(p1=p1, p2=p2), table=gexInfo))
+
 }
 
 # Ensuring command line connectivity by sourcing an argument parser
